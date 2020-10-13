f18fc94ebe1f6059427bbbdafb80fd3928d47cec
spacexjedi <ghostsavestheworld@gmail.com>
2020-10-13 15:45:28 +0000

    requirements

---
+++ main.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr #  speech
+import playsound # to play an audio file
+from gtts import gTTS # google text to speech
+import random
+from time import ctime # get time 
+import webbrowser 
+import yfinance as yf # api
+import ssl
+import certifi
+import time
+import os 
