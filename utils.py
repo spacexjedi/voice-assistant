0037b46564e13cf0cbfb8012283da258b52e0b43
spacexjedi <ghostsavestheworld@gmail.com>
2020-10-14 18:54:26 +0000

    template

---
+++ utils.py
@@ -0,0 +1,18 @@
+import speech_recognition as sr # speech
+import playsound # to play an audio 
+from gtts import gTTS # google text to speech
+import random
+from time import ctime # time
+import webbrowser 
+import ssl
+import certifi
+import time
+import os 
+from PIL import Image
+import subprocess
+import pyautogui 
+import pyttsx3
+import bs4 as bs
+import urllib.request
+import requests
+
